ac395244ccbb7afbf8f59473e80c46b3bac77b20
Scott Byrne <scott.byrne@forestry.io>
2021-02-04 14:46:21 -0400

    raw json display

--- components/page-renderer.tsx
+++ components/page-renderer.tsx
@@ -13,6 +13,12 @@ export const PageRenderer = (props: Tina.Page_Doc_Data) => {
         <div className="content">
           <BlocksRenderer blocks={blocks} />
           {_body?.raw && <p>{_body?.raw}</p>}
+          <details className="rawData">
+            <summary>Raw JSON</summary>
+            <pre className="rawDataOutput">
+              <code>{JSON.stringify(props, null, 2)}</code>
+            </pre>
+          </details>
         </div>
       </div>
       <style global jsx>{`
@@ -77,9 +83,40 @@ export const PageRenderer = (props: Tina.Page_Doc_Data) => {
 
         .content {
           flex: 1 0 auto;
-          padding: 1.5rem;
+          padding: 2rem 1.5rem;
           background: var(--mint-light);
           color: var(--blue);
+        }
+
+        .rawData {
+          margin-top: 1.5rem;
+          display: inline-block;
+          border: 1px solid var(--mint);
+          border-radius: 0.5rem;
+          background: rgba(0, 0, 0, 0.03);
+          font-size: 0.75rem;
+        }
+
+        .rawData :global(pre) {
+          padding: 0 1rem 1rem 1rem;
+        }
+
+        .rawData :global(summary) {
+          display: inline-block;
+          cursor: pointer;
+          display: block;
+          font-weight: bold;
+          padding: 1rem;
+          outline: none;
+          user-select: none;
+        }
+
+        .rawData :global(summary):hover {
+          color: var(--orange);
+        }
+
+        .rawData :global(summary)::-webkit-details-marker {
+          display: none;
         }
       `}</style>
     </>
