96c55c89e10aa78d4ff894e0f27a3fe43f520350
Scott Byrne <scott.byrne@forestry.io>
2021-02-04 14:04:21 -0400

    styling

--- components/cta.tsx
+++ components/cta.tsx
@@ -6,9 +6,27 @@ export const Cta = ({ text }) => {
       </div>
       <style jsx>{`
         .buttonGroup {
+          display: block;
+          padding: 0.25rem 0;
+          margin: -0.5rem -0.75rem 0.5rem -0.75rem;
         }
 
         .button {
+          border: none;
+          outline: none;
+          color: white;
+          border-radius: 2rem;
+          background: var(--orange);
+          font-size: 1em;
+          padding: 1rem 2rem;
+          margin: 0.5rem 0.75rem;
+          cursor: pointer;
+          transition: all 150ms ease-out;
+        }
+
+        .button:hover {
+          background: var(--orange-light);
+          box-shadow: 0 0 0 2px var(--orange-light);
         }
       `}</style>
     </>
