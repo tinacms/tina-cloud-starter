e54979b118f31476d58344fc6142d234ef1be273
Scott Byrne <scott.byrne@forestry.io>
2021-02-04 14:04:21 -0400

    styling

--- components/hero.tsx
+++ components/hero.tsx
@@ -7,12 +7,21 @@ export const Hero = ({ heading, message }) => {
       </div>
       <style jsx>{`
         .hero {
+          display: block;
         }
 
         .heading {
+          font-size: 2rem;
+          line-height: 1.4;
+          margin-top: 0;
+          margin-bottom: 1rem;
         }
 
         .message {
+          font-size: 1.25rem;
+          line-height: 1.6;
+          margin-top: 0;
+          margin-bottom: 1rem;
         }
       `}</style>
     </>
