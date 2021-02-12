3f73be09919d9cdebf92d4179e22df35ea44cda8
Scott Byrne <scott.byrne@forestry.io>
2021-02-12 13:34:25 -0400

    fix block renderer null state

--- components/blocks-renderer.tsx
+++ components/blocks-renderer.tsx
@@ -2,14 +2,16 @@ import { Hero } from "./hero";
 import { Cta } from "./cta";
 
 export const BlocksRenderer = ({ blocks }) => {
-  return blocks.map(function (block, i) {
-    switch (block.__typename) {
-      case "BlockHero_Data":
-        return <Hero {...block} />;
-      case "BlockCta_Data":
-        return <Cta {...block} />;
-      default:
-        return null;
-    }
-  });
+  return blocks
+    ? blocks.map(function (block, i) {
+        switch (block.__typename) {
+          case "BlockHero_Data":
+            return <Hero {...block} />;
+          case "BlockCta_Data":
+            return <Cta {...block} />;
+          default:
+            return null;
+        }
+      })
+    : null;
 };
