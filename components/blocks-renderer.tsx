f799c49004f00fc8e6f659f83600c6b79ef79f61
Jeff See <jeffsee.55@gmail.com>
2021-03-16 11:04:17 -0700

    Wrap blocks renderer return value in fragment

--- components/blocks-renderer.tsx
+++ components/blocks-renderer.tsx
@@ -7,16 +7,20 @@ export const BlocksRenderer = ({
 }: {
   blocks: Tina.Page_Blocks_Data[];
 }) => {
-  return blocks
-    ? blocks.map(function (block, i) {
-        switch (block.__typename) {
-          case "BlockHero_Data":
-            return <Hero {...block} />;
-          case "BlockCta_Data":
-            return <Cta {...block} />;
-          default:
-            return null;
-        }
-      })
-    : null;
+  return (
+    <>
+      {blocks
+        ? blocks.map(function (block, i) {
+            switch (block.__typename) {
+              case "BlockHero_Data":
+                return <Hero {...block} />;
+              case "BlockCta_Data":
+                return <Cta {...block} />;
+              default:
+                return null;
+            }
+          })
+        : null}
+    </>
+  );
 };
