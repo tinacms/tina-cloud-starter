549f7e73eee13c8ab0fc0b9caadbca5dc5cc6aa8
Jeff See <jeffsee.55@gmail.com>
2021-04-01 16:57:45 -0700

    Move lots of things around, simplify models and cleanup

--- .tina/schema.ts
+++ .tina/schema.ts
@@ -3,6 +3,53 @@ import { defineSchema } from "tina-graphql-gateway-cli";
 export default defineSchema({
   collections: [
     {
+      label: "Blog Posts",
+      name: "posts",
+      path: "content/posts",
+      templates: [
+        {
+          label: "Article",
+          name: "article",
+          fields: [
+            {
+              type: "text",
+              label: "Title",
+              name: "title",
+            },
+            {
+              type: "reference",
+              label: "Author",
+              name: "author",
+              collection: "authors",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: "Authors",
+      name: "authors",
+      path: "content/authors",
+      templates: [
+        {
+          label: "Author",
+          name: "author",
+          fields: [
+            {
+              type: "text",
+              label: "Name",
+              name: "name",
+            },
+            {
+              type: "text",
+              label: "Avatar",
+              name: "avatar",
+            },
+          ],
+        },
+      ],
+    },
+    {
       label: "Marketing Pages",
       name: "marketingPages",
       path: "content/marketing-pages",
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 import dynamic from "next/dynamic";
 
 const TinaWrapper = dynamic(() => import("../components/tina-wrapper"));
 
 function MyApp({ Component, pageProps }) {
-  const { route } = useRouter();
+  const { route, asPath } = useRouter();
 
   /**
    * If the route starts with /admin, we'll wrap the entire component tree
@@ -18,7 +19,29 @@ function MyApp({ Component, pageProps }) {
       </TinaWrapper>
     );
   }
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Link href={`/admin${asPath}`}>
+        <a className="editLink">Edit Page</a>
+      </Link>
+      <style jsx>{`
+        .editLink {
+          position: fixed;
+          top: 0;
+          right: 0;
+          background: var(--orange);
+          color: var(--white);
+          padding: 0.5rem 0.75rem;
+          font-weight: bold;
+          text-decoration: none;
+          display: inline-block;
+          border-bottom-left-radius: 0.5rem;
+          cursor: pointer;
+        }
+      `}</style>
+    </>
+  );
 }
 
 export default MyApp;
