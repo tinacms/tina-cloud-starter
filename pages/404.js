237deb20e377f7d90616e3a5397e158fa9b96aac
Jeff See <jeffsee.55@gmail.com>
2021-04-01 16:57:45 -0700

    Move lots of things around, simplify models and cleanup

--- pages/404.js
+++ pages/404.js
@@ -1,28 +1,10 @@
-import { GlobalStyles, PageStyles } from "../components/document-renderer";
+import { Wrapper } from "../components/wrapper";
 
 export default function FourOhFour() {
   return (
-    <>
-      <div className="header">
-        <div className="container">
-          <h1 className="title">404 - NOT FOUND</h1>
-        </div>
-      </div>
-      <div className="content">
-        <div className="container">
-          <div className="card">
-            <div className="cardBody">
-              <h2>OOPS, NOTHING TO SEE HERE...</h2>
-              <a href="/">Go back home</a>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <style global jsx>
-        {GlobalStyles}
-      </style>
-      <style jsx>{PageStyles}</style>
-    </>
+    <Wrapper>
+      <h2>OOPS, NOTHING TO SEE HERE...</h2>
+      <a href="/">Go back home</a>
+    </Wrapper>
   );
 }
