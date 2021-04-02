ad44bdc1808d7061b270895d2e7fac933f6e55d9
Jeff See <jeffsee.55@gmail.com>
2021-04-01 17:48:11 -0700

    Add message and diagram blocks, remove cta and hero ones

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { useGraphqlForms } from "tina-graphql-gateway";
-import HomePage, { request, HomeQueryResponseType } from "..";
+import HomePage, { query, HomeQueryResponseType } from "..";
 
 /**
  * HEADS UP: notice how we're importing `HomePage` from another Next.js route file?
  *
- * We're doing this so we can render the same content that you'd typically expect
- * from `getStaticProps`, except we've "hydrated" your content with Tina forms
- * automatically, so you can edit everything right there on your page.
+ * This page will be identical to the other route file except it'll come with a
+ * Tina sidebar form built for you automatically!
  */
 export default function AdminPage() {
   const [payload, isLoading] = useGraphqlForms<HomeQueryResponseType>({
-    query: request.query,
-    variables: request.variables,
+    query,
+    variables: {},
   });
 
   return isLoading ? <p>Loading...</p> : <HomePage {...payload} />;
