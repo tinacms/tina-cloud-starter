4ed7a4bbbe84088c3c02f1e4bc98128d6000f03b
Chris Davis <chris.enigmatical@gmail.com>
2021-04-21 09:41:53 -0500

    fix: Updated for section-to-collection changes
    ... and a bit of polish.
    
    * Most of the `section-to-collection` changes was centered in the `utils/index.ts` file around `variablesFromPath`.
    * I renamed `vote-for-pedro.md` to `voteForPedro.md` and changed its links because `vote-for-pedro.md` is actually not a valid content `name`.
    * I fixed an annoying `React` warning related to un`key`d elements in the `landing-page.tsx`.
    * I also better handled an issue when creating a new item in the `posts` collection that would result in an unhandled error because `author` had not been defined.
    
    Also...
    
    I set a `resolutions` for `final-form@4.20.1` due to a bug that appeared when attempting to add a new document through the `content-creator` related to how `final-form` handles `Field` Arrays and `async validate()` methods:
    https://github.com/final-form/final-form/issues/408
    
    This is actually an issue in `final-form` and we'll probably lock the version in `tinacms` (I've already spoken with DJ about it).

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -10,14 +10,14 @@ export const LandingPage = (props: LandingPage_Doc_Data) => {
             switch (block.__typename) {
               case "Message_Data":
                 return (
-                  <>
+                  <React.Fragment key={`block-${block.messageHeader}`}>
                     <h3>{block.messageHeader}</h3>
                     <Markdown>{block.messageBody}</Markdown>
-                  </>
+                  </React.Fragment>
                 );
               case "Diagram_Data":
                 return (
-                  <>
+                  <React.Fragment key={`diagram-${block.diagramID}`}>
                     <h3>{block.diagramHeading}</h3>
                     <Markdown>{block.diagramDescription}</Markdown>
                     <iframe
@@ -26,7 +26,7 @@ export const LandingPage = (props: LandingPage_Doc_Data) => {
                       height="450"
                       src={`https://whimsical.com/embed/${block.diagramID}`}
                     ></iframe>
-                  </>
+                  </React.Fragment>
                 );
               default:
                 return null;
