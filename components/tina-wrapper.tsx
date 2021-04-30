d69e0115bee7109a669b1e016bcb16393ee7c912
Jeff See <jeffsee.55@gmail.com>
2021-04-29 21:49:39 -0700

    Get code-splitting pattern working

--- components/tina-wrapper.tsx
+++ components/tina-wrapper.tsx
@@ -3,12 +3,13 @@ import { TinaCMS } from "tinacms";
 import { TinaCloudAuthWall } from "tina-graphql-gateway";
 import { SidebarPlaceholder } from "./helper-components";
 import { createClient } from "../utils";
+import { useGraphqlForms } from "tina-graphql-gateway";
 
 /**
  * This gets loaded dynamically in "pages/_app.js"
  * if you're on a route that starts with "/admin"
  */
-const TinaWrapper = ({ children }) => {
+const TinaWrapper = ({ query, variables, children }) => {
   const cms = React.useMemo(() => {
     return new TinaCMS({
       apis: {
@@ -21,7 +22,23 @@ const TinaWrapper = ({ children }) => {
     });
   }, []);
 
-  return <TinaCloudAuthWall cms={cms}>{children}</TinaCloudAuthWall>;
+  return (
+    <TinaCloudAuthWall cms={cms}>
+      <Inner query={query} variables={variables}>
+        {children}
+      </Inner>
+    </TinaCloudAuthWall>
+  );
+};
+
+const Inner = (props) => {
+  const [payload, isLoading] = useGraphqlForms({
+    query: (gql) => gql(props.query),
+    variables: props.variables || {},
+  });
+  return (
+    <>{isLoading ? <div>loading...</div> : props.children({ data: payload })}</>
+  );
 };
 
 export default TinaWrapper;
--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,39 @@
-import { useRouter } from "next/router";
 import Link from "next/link";
 import dynamic from "next/dynamic";
 
-const TinaWrapper = dynamic(() => import("../components/tina-wrapper"));
-
 function MyApp({ Component, pageProps }) {
-  const { route, asPath } = useRouter();
-
-  /**
-   * If the route starts with /admin, we'll wrap the entire component tree
-   * with Tina, meaning your non-admin routes won't contain any Tina code
-   * in their bundles.
-   */
-  if (route.startsWith("/admin") || pageProps.preview) {
+  if (pageProps.preview) {
+    const TinaWrapper = dynamic(() => import("../components/tina-wrapper"));
     return (
-      <TinaWrapper>
-        <Component {...pageProps} />
-      </TinaWrapper>
+      <>
+        <TinaWrapper query={pageProps.query} variables={pageProps.variables}>
+          {(props) => <Component {...props} />}
+        </TinaWrapper>
+        <Link href={`/api/preview`}>
+          <a className="editLink">Exit Edit Mode</a>
+        </Link>
+        <style jsx>{`
+          .editLink {
+            position: fixed;
+            top: 0;
+            right: 0;
+            background: var(--orange);
+            color: var(--white);
+            padding: 0.5rem 0.75rem;
+            font-weight: bold;
+            text-decoration: none;
+            display: inline-block;
+            border-bottom-left-radius: 0.5rem;
+            cursor: pointer;
+          }
+        `}</style>
+      </>
     );
   }
   return (
     <>
       <Component {...pageProps} />
-      <Link href={`/admin${asPath}`}>
+      <Link href={`/api/preview`}>
         <a className="editLink">Edit Page</a>
       </Link>
       <style jsx>{`
