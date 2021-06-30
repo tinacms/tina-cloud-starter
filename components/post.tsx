4bd73c729ed5499f7759c72e75cc039b77febf6e
Scott Byrne <scott.byrne@forestry.io>
2021-06-30 12:17:13 -0300

    strip existing styles

--- components/post.tsx
+++ components/post.tsx
@@ -16,32 +16,13 @@ export const BlogPost = (props: Article_Doc_Data) => {
 
 const AuthorSnippet = (props: { author: Authors_Document }) => {
   return (
-    <div className="snippet">
+    <div>
       {props.author && (
         <>
-          <img
-            className="avatar"
-            title={props.author.data.name}
-            src={props.author.data.avatar}
-          />
+          <img title={props.author.data.name} src={props.author.data.avatar} />
           <h3>By {props.author.data.name}</h3>
         </>
       )}
-      <style jsx>{`
-        .snippet {
-          display: flex;
-          align-items: center;
-        }
-        .avatar {
-          height: 50px;
-          margin-right: 10px;
-          object-fit: cover;
-          width: 50px;
-          border-radius: 50%;
-          background-size: cover;
-          background-position: center;
-        }
-      `}</style>
     </div>
   );
 };
