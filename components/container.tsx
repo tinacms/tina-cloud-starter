7bf56af115fc180a62e493f185365f0b3d365c89
Scott Byrne <scott.byrne@forestry.io>
2021-07-13 12:42:11 -0300

    tina svg, style

--- components/container.tsx
+++ components/container.tsx
@@ -8,16 +8,16 @@ export const Container = ({
 }) => {
   const sectionColor = {
     default:
-      "relative transition duration-150 ease-out text-gray-800 dark:text-gray-100 bg-white dark:bg-gray-800 body-font overflow-hidden",
-    tint: "relative transition duration-150 ease-out text-gray-700 dark:text-gray-100 bg-gradient-to-br from-gray-50 to-gray-100 dark:bg-gray-600  body-font overflow-hidden",
+      "relative transition duration-150 ease-out text-gray-800 bg-white dark:bg-gray-800 body-font overflow-hidden",
+    tint: "relative transition duration-150 ease-out text-gray-700 bg-gradient-to-br from-gray-50 to-gray-100  body-font overflow-hidden",
     primary:
-      "relative transition duration-150 ease-out text-gray-700 dark:text-gray-100 bg-blue-700 bg-gradient-to-br from-blue-500 to-blue-300 dark:from-blue-500 dark:to-blue-700 body-font overflow-hidden",
+      "relative transition duration-150 ease-out text-white bg-blue-500 bg-gradient-to-br from-blue-400 to-blue-500 body-font overflow-hidden",
   };
 
   const verticalPadding = {
-    default: "py-8",
-    small: "py-4",
-    large: "py-12",
+    default: "py-12",
+    small: "py-6",
+    large: "py-16",
   };
 
   return (
