9d72d679203ba862449f8ffff9674527378771e9
Scott Byrne <scott.byrne@forestry.io>
2021-07-14 12:05:23 -0300

    work on adding hero block

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Wrapper } from "../components/helper-components";
+import { Wrapper } from "../components/wrapper";
 
 export default function FourOhFour() {
   return (
