9cf73807ce0e57ec5f8283bb55bb80716325d759
Scott Byrne <scott.byrne@forestry.io>
2021-07-14 15:38:19 -0300

    dark mode blog list

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -19,12 +19,12 @@ export default function HomePage(
               <a
                 key={post.id}
                 href="/posts/"
-                className="group block px-8 py-10 bg-gray-50 bg-gradient-to-br from-gray-50 to-gray-100 rounded-md shadow-sm mb-6 hover:shadow-md hover:to-gray-50"
+                className="group block px-8 py-10 bg-gray-50 bg-gradient-to-br from-gray-50 to-gray-100 dark:bg-gray-700 dark:from-gray-700 dark:to-gray-600 rounded-md shadow-sm mb-6 hover:shadow-md hover:to-gray-50 dark:hover:to-gray-500"
               >
-                <h3 className="text-3xl font-semibold title-font mb-5 transition-all duration-150 ease-out group-hover:text-blue-600">
+                <h3 className="text-gray-900 dark:text-white text-3xl font-semibold title-font mb-5 transition-all duration-150 ease-out group-hover:text-blue-600 dark:group-hover:text-blue-300">
                   {post.values.title}
                 </h3>
-                <p className="mb-6 text-gray-700 group-hover:text-gray-800">
+                <p className="mb-6 text-gray-700 group-hover:text-gray-800 dark:text-gray-200 dark:group-hover:text-white">
                   Vivamus odio erat, lacinia in mauris ut, finibus blandit
                   augue. Aliquam ac ante quis diam gravida luctus. Etiam sit
                   amet mauris diam. Mauris ut eleifend erat. Donec aliquam, orci
@@ -40,11 +40,11 @@ export default function HomePage(
                       alt=""
                     />
                   </div>
-                  <p className="text-sm font-medium text-gray-600 group-hover:text-gray-800">
+                  <p className="text-sm font-medium text-gray-600 group-hover:text-gray-800 dark:text-gray-200 dark:group-hover:text-white">
                     Author Name
                   </p>
                   <span className="font-bold text-gray-200 mx-2">—</span>
-                  <p className="text-sm text-gray-400 group-hover:text-gray-500">
+                  <p className="text-sm text-gray-400 group-hover:text-gray-500 dark:text-gray-300 dark:group-hover:text-gray-150">
                     <time dateTime="2020-03-16">Mar 16, 2020</time>
                   </p>
                 </div>
