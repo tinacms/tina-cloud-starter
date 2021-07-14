ba87555c2f63b51bd98974f7c1cd268de9e50ff4
Scott Byrne <scott.byrne@forestry.io>
2021-07-14 16:42:52 -0300

    dark mode primary fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -112,7 +112,7 @@ module.exports = {
         },
         dark: {
           css: {
-            color: theme("colors.gray.300"),
+            color: theme("colors.gray.200"),
             '[class~="lead"]': { color: theme("colors.gray.400") },
             a: { color: theme("colors.gray.100") },
             strong: { color: theme("colors.gray.100") },
@@ -142,11 +142,43 @@ module.exports = {
             "tbody tr": { borderBottomColor: theme("colors.gray.800") },
           },
         },
+        primary: {
+          css: {
+            color: theme("colors.gray.50"),
+            '[class~="lead"]': { color: theme("colors.gray.400") },
+            a: { color: theme("colors.gray.100") },
+            strong: { color: theme("colors.gray.100") },
+            "ul > li::before": { backgroundColor: theme("colors.gray.700") },
+            hr: { borderColor: theme("colors.gray.800") },
+            blockquote: {
+              color: theme("colors.gray.100"),
+              borderLeftColor: theme("colors.gray.800"),
+            },
+            h1: { color: theme("colors.gray.100") },
+            h2: { color: theme("colors.gray.100") },
+            h3: { color: theme("colors.gray.100") },
+            h4: { color: theme("colors.gray.100") },
+            code: {
+              color: theme("colors.gray.100"),
+              backgroundColor: theme("colors.blue.700"),
+            },
+            "a code": { color: theme("colors.gray.100") },
+            pre: {
+              color: theme("colors.gray.200"),
+              backgroundColor: theme("colors.blue.700"),
+            },
+            thead: {
+              color: theme("colors.gray.100"),
+              borderBottomColor: theme("colors.gray.700"),
+            },
+            "tbody tr": { borderBottomColor: theme("colors.gray.800") },
+          },
+        },
       }),
     },
   },
   variants: {
-    extend: { typography: ["tint", "dark"] },
+    extend: { typography: ["tint", "dark", "primary"] },
   },
   plugins: [require("@tailwindcss/typography")],
 };
