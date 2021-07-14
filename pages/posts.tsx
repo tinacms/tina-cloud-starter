a35d74156928d08fafa0db950150a86dae2076ac
Scott Byrne <scott.byrne@forestry.io>
2021-07-14 13:05:47 -0300

    blog list design

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -19,15 +19,12 @@ export default function HomePage(
               <a
                 key={post.id}
                 href="/posts/"
-                className="group block px-8 py-10 bg-gray-50 bg-gradient-to-br from-gray-50 to-gray-100 rounded-md shadow-sm mb-6 hover:shadow-md"
+                className="group block px-8 py-10 bg-gray-50 bg-gradient-to-br from-gray-50 to-gray-100 rounded-md shadow-sm mb-6 hover:shadow-md hover:to-gray-50"
               >
-                <div className="flex space-x-2 text-sm text-gray-400 group-hover:text-gray-500 mb-2">
-                  <time dateTime="2020-03-16">Mar 16, 2020</time>
-                </div>
-                <h3 className="text-3xl font-semibold title-font mb-4 transition-all duration-150 ease-out group-hover:text-blue-600">
+                <h3 className="text-3xl font-semibold title-font mb-5 transition-all duration-150 ease-out group-hover:text-blue-600">
                   {post.values.title}
                 </h3>
-                <p className="mb-4 text-gray-700 group-hover:text-gray-800">
+                <p className="mb-6 text-gray-700 group-hover:text-gray-800">
                   Vivamus odio erat, lacinia in mauris ut, finibus blandit
                   augue. Aliquam ac ante quis diam gravida luctus. Etiam sit
                   amet mauris diam. Mauris ut eleifend erat. Donec aliquam, orci
@@ -43,8 +40,12 @@ export default function HomePage(
                       alt=""
                     />
                   </div>
-                  <p className="text-sm font-medium text-gray-700 group-hover:text-gray-800">
+                  <p className="text-sm font-medium text-gray-600 group-hover:text-gray-800">
                     Author Name
+                  </p>
+                  <span className="font-bold text-gray-200 mx-2">—</span>
+                  <p className="text-sm text-gray-400 group-hover:text-gray-500">
+                    <time dateTime="2020-03-16">Mar 16, 2020</time>
                   </p>
                 </div>
               </a>
