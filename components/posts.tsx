98d54cb4755f9ee2b25840de2d0f15d5389f5639
Scott Byrne <scott.byrne@forestry.io>
2021-07-15 16:51:53 -0300

    post hover link indicator

--- components/posts.tsx
+++ components/posts.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Markdown from "react-markdown";
+import { BsArrowRight } from "react-icons/bs";
 
 export const Posts = ({ data }) => {
   return (
@@ -17,7 +18,10 @@ export const Posts = ({ data }) => {
               className="group block px-8 py-10 mb-8 bg-gray-50 bg-gradient-to-br from-gray-50 to-gray-100 dark:bg-gray-700 dark:from-gray-800 dark:to-gray-700 rounded-md shadow-sm transition-all duration-150 ease-out hover:shadow-md hover:to-gray-50 dark:hover:to-gray-600"
             >
               <h3 className="text-gray-900 dark:text-white text-3xl font-semibold title-font mb-5 transition-all duration-150 ease-out group-hover:text-blue-600 dark:group-hover:text-blue-300">
-                {post.values.title}
+                {post.values.title}{" "}
+                <span className="inline-block opacity-0 group-hover:opacity-100 transition-all duration-300 ease-out">
+                  <BsArrowRight className="inline-block h-8 -mt-1 ml-1 w-auto opacity-70" />
+                </span>
               </h3>
               <div className="prose dark:prose-dark prose-lg w-full max-w-none mb-5">
                 <Markdown>{post.values.excerpt}</Markdown>
