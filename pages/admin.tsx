083c4d26a25143148a52a5900fd24db33e5b7c64
Scott Byrne <scott.byrne@forestry.io>
2021-07-15 15:34:37 -0300

    new loading state

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { Container } from "../components/container";
+import { Section } from "../components/section";
 import { Wrapper } from "../components/wrapper";
 import { useEditState } from "../utils/editState";
 
@@ -13,9 +14,11 @@ const GoToEditPage: React.FC = () => {
   }, []);
   return (
     <Wrapper>
-      <Container size="large prose prose-xl">
-        <h2>Going into edit mode...</h2>
-      </Container>
+      <Section className="flex-1">
+        <Container size="large prose prose-xl">
+          <h2>Going into edit mode...</h2>
+        </Container>
+      </Section>
     </Wrapper>
   );
 };
