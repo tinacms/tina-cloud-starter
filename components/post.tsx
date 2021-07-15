5530474abf38a93e5e772e3cb2e09a65feaf8d37
Scott Byrne <scott.byrne@forestry.io>
2021-07-15 14:34:59 -0300

    post details, date, excerpt

--- components/post.tsx
+++ components/post.tsx
@@ -1,28 +1,49 @@
-import ReactMarkdown from "react-markdown";
-import type {
-  Authors_Document,
-  Article_Doc_Data,
-} from "../.tina/__generated__/types";
+import React from "react";
+import Markdown from "react-markdown";
+import { Container } from "./container";
+import { Section } from "./section";
 
-export const BlogPost = (props: Article_Doc_Data) => {
+export const Post = ({ data }) => {
   return (
-    <>
-      <h1>{props.title}</h1>
-      <AuthorSnippet author={props.author} />
-      <ReactMarkdown>{props._body}</ReactMarkdown>
-    </>
-  );
-};
+    <Section className="flex-1">
+      <Container className={`flex-1 max-w-4xl`} size="large">
+        <h2
+          className={`w-full relative	mb-8 text-6xl font-extrabold tracking-normal text-center title-font`}
+        >
+          <span
+            className={`bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-blue-600 dark:from-blue-300 dark:to-blue-500`}
+          >
+            {data.values.title}
+          </span>
+        </h2>
 
-const AuthorSnippet = (props: { author: Authors_Document }) => {
-  return (
-    <div>
-      {props.author && (
-        <>
-          <img title={props.author.data.name} src={props.author.data.avatar} />
-          <h3>By {props.author.data.name}</h3>
-        </>
-      )}
-    </div>
+        <div className="flex items-center justify-center mb-16">
+          {data.data.author && (
+            <>
+              <div className="flex-shrink-0 mr-4">
+                <img
+                  className="h-14 w-14 rounded-full shadow-sm"
+                  src={data.data.author.data.avatar}
+                  alt={data.data.author.data.name}
+                />
+              </div>
+              <p className="text-base font-medium text-gray-600 group-hover:text-gray-800 dark:text-gray-200 dark:group-hover:text-white">
+                {data.data.author.data.name}
+              </p>
+              <span className="font-bold text-gray-200 dark:text-gray-500 mx-2">
+                —
+              </span>
+            </>
+          )}
+          <p className="text-base text-gray-400 group-hover:text-gray-500 dark:text-gray-300 dark:group-hover:text-gray-150">
+            <time dateTime="2020-03-16">{data.values.date}</time>
+          </p>
+        </div>
+
+        <div className="prose dark:prose-dark  w-full max-w-none">
+          <Markdown>{data.values._body}</Markdown>
+        </div>
+      </Container>
+    </Section>
   );
 };
