a0718b06a78f5679bf33087c4c36de3c80802f3c
Scott Byrne <scott.byrne@forestry.io>
2021-07-15 10:58:51 -0300

    author data, post link

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -15,10 +15,11 @@ export default function HomePage(
         <Container size="large">
           {posts.map((item) => {
             const post = item.node;
+            console.log(post);
             return (
               <a
                 key={post.id}
-                href="/posts/"
+                href={`/posts/` + post.sys.filename}
                 className="group block px-8 py-10 bg-gray-50 bg-gradient-to-br from-gray-50 to-gray-100 dark:bg-gray-700 dark:from-gray-800 dark:to-gray-700 rounded-md shadow-sm mb-6 transition-all duration-150 ease-out hover:shadow-md hover:to-gray-50 dark:hover:to-gray-600"
               >
                 <h3 className="text-gray-900 dark:text-white text-3xl font-semibold title-font mb-5 transition-all duration-150 ease-out group-hover:text-blue-600 dark:group-hover:text-blue-300">
@@ -36,12 +37,12 @@ export default function HomePage(
                   <div className="flex-shrink-0 mr-2">
                     <img
                       className="h-10 w-10 rounded-full shadow-sm"
-                      src="https://source.unsplash.com/rDEOVtE7vOs/128x128"
-                      alt=""
+                      src={post.data.author.data.avatar}
+                      alt={post.data.author.data.name}
                     />
                   </div>
                   <p className="text-sm font-medium text-gray-600 group-hover:text-gray-800 dark:text-gray-200 dark:group-hover:text-white">
-                    Author Name
+                    {post.data.author.data.name}
                   </p>
                   <span className="font-bold text-gray-200 dark:text-gray-500 mx-2">
                     —
@@ -66,6 +67,19 @@ export const query = `#graphql
         node {
           id
           values
+          data {
+            author {
+							... on AuthorsDocument {
+                data {                  
+									name
+                	avatar
+                }
+              }
+            }
+          }
+          sys {
+            filename
+          }
         }
       }
     }
