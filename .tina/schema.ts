148b9cb81622ffe85b41a3d13ba9d8de5ed635a9
Scott Byrne <scott.byrne@forestry.io>
2021-07-19 14:54:13 -0300

    default feature

--- .tina/schema.ts
+++ .tina/schema.ts
@@ -214,6 +214,17 @@ export default unstable_defineSchema({
                   label: "Feature Items",
                   name: "items",
                   list: true,
+                  ui: {
+                    defaultItem: {
+                      title: "Here's Another Feature",
+                      text: "This is where you might talk about the feature, if this wasn't just filler text.",
+                      icon: {
+                        color: "blue",
+                        style: "float",
+                        name: "",
+                      },
+                    },
+                  },
                   fields: [
                     {
                       type: "object",
