73eeb8034d4f350e2cd88ddf0586a304ab1d68ab
Jack Bevis <jack.bevis@forestry.io>
2021-07-19 16:57:25 -0600

    add media support

--- components/tina-wrapper.tsx
+++ components/tina-wrapper.tsx
@@ -1,47 +1,32 @@
-import React from "react";
-import { TinaCMS } from "tinacms";
-import { TinaCloudAuthWall } from "tina-graphql-gateway";
-import { Placeholder } from "./placeholder";
-import { createClient } from "../utils";
-import { unstable_useGraphQLForms } from "tina-graphql-gateway";
-import { Loading } from "./loading";
+import { TinaCloudProvider } from 'tina-graphql-gateway'
+import React from 'react'
+import { useGraphqlForms } from 'tina-graphql-gateway'
+import { Loading } from './loading'
+import { TinaCloudCloudinaryMediaStore } from 'next-tinacms-cloudinary'
 
 /**
  * This gets loaded dynamically in "pages/_app.js"
  * if you're on a route that starts with "/admin"
  */
 const TinaWrapper = (props) => {
-  const cms = React.useMemo(() => {
-    return new TinaCMS({
-      apis: {
-        tina: createClient(),
-      },
-      sidebar: {
-        placeholder: Placeholder,
-      },
-      enabled: true,
-    });
-  }, []);
-
-  /** Disables the TinaCMS "Media Manager" */
-  cms.plugins.all("screen").forEach((plugin) => {
-    if (plugin.name === "Media Manager") {
-      cms.plugins.remove(plugin);
-    }
-  });
-
   return (
-    <TinaCloudAuthWall cms={cms}>
+    <TinaCloudProvider
+      clientId={process.env.NEXT_PUBLIC_TINA_CLIENT_ID}
+      branch="main"
+      isLocalClient={Boolean(Number(process.env.NEXT_PUBLIC_USE_LOCAL_CLIENT))}
+      organization={process.env.NEXT_PUBLIC_ORGANIZATION_NAME}
+      mediaStore={TinaCloudCloudinaryMediaStore}
+    >
       <Inner {...props} />
-    </TinaCloudAuthWall>
-  );
-};
+    </TinaCloudProvider>
+  )
+}
 
 const Inner = (props) => {
-  const [payload, isLoading] = unstable_useGraphQLForms({
+  const [payload, isLoading] = useGraphqlForms({
     query: (gql) => gql(props.query),
     variables: props.variables || {},
-  });
+  })
   return (
     <>
       {isLoading ? (
@@ -51,7 +36,7 @@ const Inner = (props) => {
         props.children({ ...props, data: payload })
       )}
     </>
-  );
-};
+  )
+}
 
-export default TinaWrapper;
+export default TinaWrapper
