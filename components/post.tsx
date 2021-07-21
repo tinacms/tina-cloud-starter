29e14581dab6c43804c113f85cd4bd0dc4148c3f
Scott Byrne <scott.byrne@forestry.io>
2021-07-21 11:15:04 -0300

    themeable blog

--- components/post.tsx
+++ components/post.tsx
@@ -2,8 +2,24 @@ import React from "react";
 import Markdown from "react-markdown";
 import { Container } from "./container";
 import { Section } from "./section";
+import { ThemeContext } from "./theme";
 
 export const Post = ({ data }) => {
+  const theme = React.useContext(ThemeContext);
+  const titleColorClasses = {
+    blue: "from-blue-400 to-blue-600 dark:from-blue-300 dark:to-blue-500",
+    teal: "from-teal-400 to-teal-600 dark:from-teal-300 dark:to-teal-500",
+    green: "from-green-400 to-green-600",
+    red: "from-red-400 to-red-600",
+    pink: "from-pink-300 to-pink-500",
+    purple:
+      "from-purple-400 to-purple-600 dark:from-purple-300 dark:to-purple-500",
+    orange:
+      "from-orange-300 to-orange-600 dark:from-orange-200 dark:to-orange-500",
+    yellow:
+      "from-yellow-400 to-yellow-500 dark:from-yellow-300 dark:to-yellow-500",
+  };
+
   return (
     <Section className="flex-1">
       <Container className={`flex-1 max-w-4xl pb-2`} size="large">
@@ -11,7 +27,9 @@ export const Post = ({ data }) => {
           className={`w-full relative	mb-8 text-6xl font-extrabold tracking-normal text-center title-font`}
         >
           <span
-            className={`bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-blue-600 dark:from-blue-300 dark:to-blue-500`}
+            className={`bg-clip-text text-transparent bg-gradient-to-r ${
+              titleColorClasses[theme.color]
+            }`}
           >
             {data.data.title}
           </span>
