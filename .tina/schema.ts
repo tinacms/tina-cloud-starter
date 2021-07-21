1f7f19f01de73d472666b8483c5ed2e7a9d83e1d
Scott Byrne <scott.byrne@forestry.io>
2021-07-21 17:50:52 -0300

    default feature icon color

--- .tina/schema.ts
+++ .tina/schema.ts
@@ -4,7 +4,7 @@ const defaultFeature = {
   title: "Here's Another Feature",
   text: "This is where you might talk about the feature, if this wasn't just filler text.",
   icon: {
-    color: "blue",
+    color: "",
     style: "float",
     name: "",
   },
