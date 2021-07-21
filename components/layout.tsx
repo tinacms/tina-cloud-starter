e95fa9d0dc5b8223fb84f20eac4d46f037def9e3
Scott Byrne <scott.byrne@forestry.io>
2021-07-21 18:57:33 -0300

    font family options

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,11 +11,41 @@ export const Layout = ({ rawData = "", data = layoutData, children }) => {
       <Head>
         <title>Tina</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        {data.theme.font === "nunito" && (
+          <>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link
+              rel="preconnect"
+              href="https://fonts.gstatic.com"
+              crossorigin
+            />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,700;0,800;1,400;1,700;1,800&display=swap"
+              rel="stylesheet"
+            />
+          </>
+        )}
+        {data.theme.font === "lato" && (
+          <>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link
+              rel="preconnect"
+              href="https://fonts.gstatic.com"
+              crossorigin
+            />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap"
+              rel="stylesheet"
+            />
+          </>
+        )}
       </Head>
       <Theme data={data?.theme}>
         <div
           className={`min-h-screen flex flex-col ${
-            data.theme.font === "geometric" ? "font-geometric" : "font-sans"
+            data.theme.font === "nunito" && "font-nunito"
+          } ${data.theme.font === "lato" && "font-lato"} ${
+            data.theme.font === "sans" && "font-sans"
           }`}
         >
           <Header data={data?.header} />
