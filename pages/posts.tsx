824959bff88ad4cb0abf0653f15a57619faccfc5
Jeff See <jeffsee.55@gmail.com>
2021-07-22 17:43:53 -0700

    Update Next.js setup components

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,9 +1,10 @@
+import { getStaticPropsForTina } from "tinacms";
 import { Container } from "../components/container";
 import { Section } from "../components/section";
 import { Posts } from "../components/posts";
-import type { PostsConnection } from "../.tina/__generated__/types";
-import { createLocalClient, AsyncReturnType } from "../utils";
+import { AsyncReturnType } from "../utils";
 import { layoutQueryFragment } from "../components/layout";
+import type { PostsConnection } from "../.tina/__generated__/types";
 
 export default function HomePage(
   props: AsyncReturnType<typeof getStaticProps>["props"]
@@ -19,46 +20,39 @@ export default function HomePage(
   );
 }
 
-export const query = `#graphql
-  query PageQuery {
-    ${layoutQueryFragment}
-    getPostsList {
-      edges {
-        node {
-          id
-          values
-          data {
-            author {
-							... on AuthorsDocument {
-                data {                  
-									name
-                	avatar
+export const getStaticProps = async () => {
+  const tinaProps = (await getStaticPropsForTina({
+    query: `#graphql
+      query PageQuery {
+        ${layoutQueryFragment}
+        getPostsList {
+          edges {
+            node {
+              id
+              values
+              data {
+                author {
+                  ... on AuthorsDocument {
+                    data {
+                      name
+                      avatar
+                    }
+                  }
                 }
+              }
+              sys {
+                filename
               }
             }
           }
-          sys {
-            filename
-          }
         }
       }
-    }
-  }
-`;
-
-export const getStaticProps = async () => {
-  const client = createLocalClient();
-  const data = await client.request<{
-    getPostsList: PostsConnection;
-  }>(query, {
-    variables: {},
-  });
+    `,
+  })) as { data: { getPostsList: PostsConnection } };
 
   return {
     props: {
-      data: data,
-      query: query,
-      variables: {},
+      ...tinaProps,
     },
   };
 };
