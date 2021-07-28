f2f6bce7cc0ad41acba11ad535827cd3ded31a4d
Logan Anderson <landerson2@upei.ca>
2021-07-28 15:04:11 -0300

    added an exit-admin path

---
+++ pages/exit-admin.tsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { Container } from "../components/container";
+import { Section } from "../components/section";
+import { Layout } from "../components/layout";
+import { useEditState } from "tinacms/dist/edit-state";
+
+const GoToEditPage: React.FC = () => {
+  const { setEdit } = useEditState();
+  const router = useRouter();
+  useEffect(() => {
+    setEdit(false);
+    router.back();
+  }, []);
+  return (
+    <Layout>
+      <Section className="flex-1">
+        <Container size="large prose prose-xl">
+          <h2>Exiting edit mode...</h2>
+        </Container>
+      </Section>
+    </Layout>
+  );
+};
+
+export default GoToEditPage;
