87bbd29890494d3ae09226811b98f67c184492c8
Logan Anderson <landerson2@upei.ca>
2021-08-17 14:02:56 -0300

    added admin page

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,25 +1,79 @@
-import { useEffect } from "react";
 import { useRouter } from "next/router";
+import { Button } from "tinacms";
 import { Container } from "../components/container";
 import { Section } from "../components/section";
-import { Layout } from "../components/layout";
 import { useEditState } from "tinacms/dist/edit-state";
+import { ThemeContext } from "../components/theme";
+import React from "react";
+import {
+  buttonColorClasses,
+  invertedButtonColorClasses,
+} from "../components/actions";
 
 const GoToEditPage: React.FC = () => {
-  const { setEdit } = useEditState();
+  const { edit, setEdit } = useEditState();
   const router = useRouter();
-  useEffect(() => {
-    setEdit(true);
-    router.back();
-  }, []);
+  const theme = React.useContext(ThemeContext);
   return (
-    <Layout>
-      <Section className="flex-1">
-        <Container size="large prose prose-xl">
-          <h2>Going into edit mode...</h2>
-        </Container>
-      </Section>
-    </Layout>
+    <Section className="flex-1">
+      <Container size="large prose prose-xl">
+        <h1 className="w-full relative	mb-10 text-5xl font-extrabold tracking-normal leading-tight title-font">
+          Welcome to the Admin page
+        </h1>
+        {!edit && (
+          <>
+            <p className="mt-5 mb-5">
+              Thanks for using{" "}
+              <a className="underline" href="https://tina.io">
+                TinaCMS!
+              </a>{" "}
+              Please login with Tina Cloud, and then navigate your site in edit
+              mode.
+            </p>
+          </>
+        )}
+        {edit && (
+          <>
+            <p className="mt-5 mb-5">
+              You are now logged in and in edit mode! You can now to to the home
+              page and begin to make content changes
+            </p>
+          </>
+        )}
+        <div className="flex flex-wrap items-center gap-y-4 gap-x-6 ">
+          <button
+            className={`z-10 relative flex items-center px-7 py-3 font-semibold text-lg transition duration-150 ease-out  rounded transform focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 whitespace-nowrap ${
+              invertedButtonColorClasses[theme.color]
+            }`}
+            onClick={() => {
+              router.push("/");
+            }}
+          >
+            Go to the home page
+          </button>
+          <button
+            className={`z-10 relative flex items-center px-7 py-3 font-semibold text-lg transition duration-150 ease-out  rounded transform focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 whitespace-nowrap ${
+              buttonColorClasses[theme.color]
+            }`}
+            onClick={() => {
+              setEdit(!edit);
+            }}
+          >
+            {edit ? "Logout of TinaCMS" : "Login with TinaCMS"}
+          </button>
+        </div>
+        Or{" "}
+        <span
+          className="underline hover:cursor-pointer"
+          onClick={() => {
+            router.back();
+          }}
+        >
+          Click here
+        </span>{" "}
+        to go back
+      </Container>
+    </Section>
   );
 };
 
