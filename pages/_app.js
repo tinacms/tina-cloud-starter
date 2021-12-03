0ea3a4351b099ba1ad114ceb37699b8faf2758da
Jeff See <jeffsee.55@gmail.com>
2021-12-03 08:08:17 -0800

    Enable branching

--- pages/_app.js
+++ pages/_app.js
@@ -26,6 +26,7 @@ const App = ({ Component, pageProps }) => {
               import("react-tinacms-editor").then(({ MarkdownFieldPlugin }) => {
                 cms.plugins.add(MarkdownFieldPlugin);
               });
+              cms.flags.set("branch-switcher", true);
             }}
             documentCreatorCallback={{
               /**
