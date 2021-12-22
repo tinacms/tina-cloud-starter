9b89f75892c67dd4037f65b156217d9754e98240
Jeff See <jeffsee.55@gmail.com>
2021-12-22 10:19:53 -0800

    WIP - testing out the new tailwind changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,7 +32,7 @@ const App = ({ Component, pageProps }) => {
               /**
                * Enables `tina-admin` specific features in the Tina Sidebar
                */
-              cms.flags.set("tina-admin", false);
+              cms.flags.set("tina-admin", true);
             }}
             documentCreatorCallback={{
               /**
