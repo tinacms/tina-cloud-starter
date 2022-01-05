7fd360bf5e185d155b22cfbcc107acd0f3cbee82
Corey <18439866+cjmcintyre@users.noreply.github.com>
2022-01-05 11:43:56 +1100

    Update [[...tina]].tsx to use dynamic import

--- pages/admin/[[...tina]].tsx
+++ pages/admin/[[...tina]].tsx
@@ -1,2 +1,5 @@
-import { TinaAdmin } from "tinacms";
+import dynamic from 'next/dynamic';
+
+const TinaAdmin = dynamic(() => import('tinacms').then((module) => module.TinaAdmin));
+
 export default TinaAdmin;
