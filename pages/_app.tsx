6906bff9db550a7ec76d416271ed4999ede2f344
Chris Davis <chris.enigmatical@gmail.com>
2022-02-09 12:11:15 -0600

    Bumps tinacms packages to latest (cli @ 0.60.5 & tinacms @ 0.66.3)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -44,7 +44,7 @@ const App = ({ Component, pageProps }) => {
               /**
                * Enables `tina-admin` specific features in the Tina Sidebar
                */
-              cms.flags.set("tina-admin", false);
+              cms.flags.set("tina-admin", true);
 
               return cms;
             }}
