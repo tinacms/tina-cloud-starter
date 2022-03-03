67265088cf6fd777f64c6831ec423ea15e7632d5
Logan Anderson <landerson2@upei.ca>
2022-03-03 10:16:14 -0500

    update to use new pattern

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,125 +1,17 @@
 import "../styles.css";
-import dynamic from "next/dynamic";
-import { TinaEditProvider } from "tinacms/dist/edit-state";
 import { Layout } from "../components/layout";
-// @ts-ignore FIXME: default export needs to be 'ComponentType<{}>
-const TinaCMS = dynamic(() => import("tinacms"), { ssr: false });
-import { useTina } from "tinacms/dist/edit-state";
-
-// Register forms at the root, and send the liveData down to pages
-// in edit-mode
-const QueryContainer = (props: { query; variables; data; children }) => {
-  const { data } = useTina({
-    query: props.query,
-    variables: props.variables,
-    data: props.data,
-  });
-  return props.children({ data });
-};
-
-const branch = "main";
-const apiURL =
-  process.env.NODE_ENV == "development"
-    ? "http://localhost:4001/graphql"
-    : `https://content.tinajs.io/content/${process.env.NEXT_PUBLIC_TINA_CLIENT_ID}/github/${branch}`;
+import TinaProvider from "../.tina/components/TinaDynamicProvider";
 
 const App = ({ Component, pageProps }) => {
   return (
-    <>
-      <TinaEditProvider
-        showEditButton={true}
-        editMode={
-          <TinaCMS
-            apiURL={apiURL}
-            mediaStore={async () => {
-              const pack = await import("next-tinacms-cloudinary");
-              return pack.TinaCloudCloudinaryMediaStore;
-            }}
-            cmsCallback={(cms) => {
-              /**
-               * Enables experimental branch switcher
-               */
-              cms.flags.set("branch-switcher", true);
-
-              /**
-               * Enables `tina-admin` specific features in the Tina Sidebar
-               */
-              cms.flags.set("tina-admin", true);
-
-              /**
-               * When `tina-admin` is enabled, this plugin configures contextual editing for collections
-               */
-              import("tinacms").then(({ RouteMappingPlugin }) => {
-                const RouteMapping = new RouteMappingPlugin(
-                  (collection, document) => {
-                    if (["authors", "global"].includes(collection.name)) {
-                      return undefined;
-                    }
-                    if (["pages"].includes(collection.name)) {
-                      if (document.sys.filename === "home") {
-                        return `/`;
-                      }
-                      if (document.sys.filename === "about") {
-                        return `/about`;
-                      }
-                      return undefined;
-                    }
-                    return `/${collection.name}/${document.sys.filename}`;
-                  }
-                );
-                cms.plugins.add(RouteMapping);
-              });
-
-              return cms;
-            }}
-            documentCreatorCallback={{
-              /**
-               * After a new document is created, redirect to its location
-               */
-              onNewDocument: ({ collection: { slug }, breadcrumbs }) => {
-                const relativeUrl = `/${slug}/${breadcrumbs.join("/")}`;
-                return (window.location.href = relativeUrl);
-              },
-              /**
-               * Only allows documents to be created to the `Blog Posts` Collection
-               */
-              filterCollections: (options) => {
-                return options.filter(
-                  (option) => option.label === "Blog Posts"
-                );
-              },
-            }}
-            formifyCallback={({ formConfig, createForm, createGlobalForm }) => {
-              if (formConfig.id === "getGlobalDocument") {
-                return createGlobalForm(formConfig);
-              }
-
-              return createForm(formConfig);
-            }}
-          >
-            <QueryContainer {...pageProps} key={pageProps.query}>
-              {(livePageProps) => (
-                <>
-                  <Layout
-                    rawData={livePageProps}
-                    data={livePageProps.data?.getGlobalDocument?.data}
-                  >
-                    <Component {...livePageProps} />
-                  </Layout>
-                </>
-              )}
-            </QueryContainer>
-          </TinaCMS>
-        }
+    <TinaProvider>
+      <Layout
+        rawData={pageProps}
+        data={pageProps?.data?.getGlobalDocument?.data}
       >
-        <Layout
-          rawData={pageProps}
-          data={pageProps.data?.getGlobalDocument?.data}
-        >
-          <Component {...pageProps} />
-        </Layout>
-      </TinaEditProvider>
-    </>
+        <Component {...pageProps} />
+      </Layout>
+    </TinaProvider>
   );
 };
 
