404fb096cede37225bacbd5f6bf61965b5bc16c9
James O'Halloran <james.p.ohalloran@gmail.com>
2022-04-20 20:09:50 -0300

    group colors

--- components/icon.tsx
+++ components/icon.tsx
@@ -87,29 +87,45 @@ const heroIconOptions = {
   aperture: FiAperture,
   tina: TinaIconSvg,
 };
-
-const iconCircleColorClass = {
-  blue: "bg-blue-400 dark:bg-blue-500 text-blue-50",
-  teal: "bg-teal-400 dark:bg-teal-500 text-teal-50",
-  green: "bg-green-400 dark:bg-green-500 text-green-50",
-  red: "bg-red-400 dark:bg-red-500 text-red-50",
-  pink: "bg-pink-400 dark:bg-pink-500 text-pink-50",
-  purple: "bg-purple-400 dark:bg-purple-500 text-purple-50",
-  orange: "bg-orange-400 dark:bg-orange-500 text-orange-50",
-  yellow: "bg-yellow-400 dark:bg-yellow-500 text-yellow-50",
-};
-
-const iconColorClass = {
-  blue: "text-blue-400",
-  teal: "text-teal-400",
-  green: "text-green-400",
-  red: "text-red-400",
-  pink: "text-pink-400",
-  purple: "text-purple-400",
-  orange: "text-orange-400",
-  yellow: "text-yellow-400",
-  white: "text-white opacity-80",
-};
+const iconColorClass: { [name: string]: { regular: string; circle: string } } =
+  {
+    blue: {
+      regular: "text-blue-400",
+      circle: "bg-blue-400 dark:bg-blue-500 text-blue-50",
+    },
+    teal: {
+      regular: "text-teal-400",
+      circle: "bg-teal-400 dark:bg-teal-500 text-teal-50",
+    },
+    green: {
+      regular: "text-green-400",
+      circle: "bg-green-400 dark:bg-green-500 text-green-50",
+    },
+    red: {
+      regular: "text-red-400",
+      circle: "bg-red-400 dark:bg-red-500 text-red-50",
+    },
+    pink: {
+      regular: "text-pink-400",
+      circle: "bg-pink-400 dark:bg-pink-500 text-pink-50",
+    },
+    purple: {
+      regular: "text-purple-400",
+      circle: "bg-purple-400 dark:bg-purple-500 text-purple-50",
+    },
+    orange: {
+      regular: "text-orange-400",
+      circle: "bg-orange-400 dark:bg-orange-500 text-orange-50",
+    },
+    yellow: {
+      regular: "text-yellow-400",
+      circle: "bg-yellow-400 dark:bg-yellow-500 text-yellow-50",
+    },
+    white: {
+      regular: "text-white opacity-80",
+      circle: "bg-white-400 dark:bg-white-500 text-white-50",
+    },
+  };
 
 const iconSizeClass = {
   small: "w-8 h-8",
@@ -145,24 +161,23 @@ export const Icon = ({
       : data.color
     : theme.color;
 
-  const iconColorClasses =
-    iconColorClass[
-      parentColor === "primary" &&
-      (iconColor === theme.color || iconColor === "primary")
-        ? "white"
-        : iconColor
-    ];
-
   if (data.style == "circle") {
     return (
       <div
         data-tinafield={tinaField}
-        className={`relative z-10 inline-flex items-center justify-center flex-shrink-0 ${iconSizeClass[iconSize]} rounded-full ${iconCircleColorClass[iconColor]} ${className}`}
+        className={`relative z-10 inline-flex items-center justify-center flex-shrink-0 ${iconSizeClass[iconSize]} rounded-full ${iconColorClass[iconColor].circle} ${className}`}
       >
         <IconSVG className="w-2/3 h-2/3" />
       </div>
     );
   } else {
+    const iconColorClasses =
+      iconColorClass[
+        parentColor === "primary" &&
+        (iconColor === theme.color || iconColor === "primary")
+          ? "white"
+          : iconColor
+      ].regular;
     return (
       <IconSVG
         data-tinafield={tinaField}
