a71e682ec3e591c5253ef21769dea2dbb1bbef8c
James O'Halloran <james.p.ohalloran@gmail.com>
2022-04-20 19:53:27 -0300

    refactor icons, fix circle in header/footer

--- components/icon.tsx
+++ components/icon.tsx
@@ -88,6 +88,36 @@ const heroIconOptions = {
   tina: TinaIconSvg,
 };
 
+const iconCircleColorClass = {
+  blue: "bg-blue-400 dark:bg-blue-500 text-blue-50",
+  teal: "bg-teal-400 dark:bg-teal-500 text-teal-50",
+  green: "bg-green-400 dark:bg-green-500 text-green-50",
+  red: "bg-red-400 dark:bg-red-500 text-red-50",
+  pink: "bg-pink-400 dark:bg-pink-500 text-pink-50",
+  purple: "bg-purple-400 dark:bg-purple-500 text-purple-50",
+  orange: "bg-orange-400 dark:bg-orange-500 text-orange-50",
+  yellow: "bg-yellow-400 dark:bg-yellow-500 text-yellow-50",
+};
+
+const iconColorClass = {
+  blue: "text-blue-400",
+  teal: "text-teal-400",
+  green: "text-green-400",
+  red: "text-red-400",
+  pink: "text-pink-400",
+  purple: "text-purple-400",
+  orange: "text-orange-400",
+  yellow: "text-yellow-400",
+  white: "text-white opacity-80",
+};
+
+const iconSizeClass = {
+  small: "w-8 h-8",
+  medium: "w-12 h-12",
+  large: "w-14 h-14",
+  custom: "",
+};
+
 export const Icon = ({
   data,
   parentColor = "",
@@ -95,43 +125,19 @@ export const Icon = ({
   tinaField = "",
 }) => {
   const theme = React.useContext(ThemeContext);
-  const IconSVG = React.useMemo(() => {
-    const iconOptions =
-      theme.icon === "boxicon" ? biIconOptions : heroIconOptions;
-    if (!data.name || data.name === "" || !iconOptions[data.name]) {
-      return randomProperty(iconOptions);
-    } else {
-      return iconOptions[data.name];
-    }
-  }, [data.name, theme.icon]);
+
+  const iconOptions =
+    theme.icon === "boxicon" ? biIconOptions : heroIconOptions;
+
+  const IconSVG =
+    !data.name || !iconOptions[data.name]
+      ? Object.keys(iconOptions)[0]
+      : iconOptions[data.name];
 
   const iconSize = data.size ? data.size : "large";
 
   /* Full class strings are required for Tailwind's just-in-time mode,
      I would love a better solution that doesn't require so much repetition */
-
-  const iconCircleColorClass = {
-    blue: "bg-blue-400 dark:bg-blue-500 text-blue-50",
-    teal: "bg-teal-400 dark:bg-teal-500 text-teal-50",
-    green: "bg-green-400 dark:bg-green-500 text-green-50",
-    red: "bg-red-400 dark:bg-red-500 text-red-50",
-    pink: "bg-pink-400 dark:bg-pink-500 text-pink-50",
-    purple: "bg-purple-400 dark:bg-purple-500 text-purple-50",
-    orange: "bg-orange-400 dark:bg-orange-500 text-orange-50",
-    yellow: "bg-yellow-400 dark:bg-yellow-500 text-yellow-50",
-  };
-
-  const iconColorClass = {
-    blue: "text-blue-400",
-    teal: "text-teal-400",
-    green: "text-green-400",
-    red: "text-red-400",
-    pink: "text-pink-400",
-    purple: "text-purple-400",
-    orange: "text-orange-400",
-    yellow: "text-yellow-400",
-    white: "text-white opacity-80",
-  };
 
   const iconColor = data.color
     ? data.color === "primary"
@@ -147,48 +153,23 @@ export const Icon = ({
         : iconColor
     ];
 
-  const iconSizeClass = {
-    small: "w-8 h-8",
-    medium: "w-12 h-12",
-    large: "w-14 h-14",
-    custom: "",
-  };
-
-  const Component = React.useMemo(() => {
-    if (!IconSVG) return null;
-    if (data.style == "circle") {
-      return (
-        <div
-          data-tinafield={tinaField}
-          className={`relative z-10 inline-flex items-center justify-center flex-shrink-0 ${iconSizeClass[iconSize]} rounded-full ${iconCircleColorClass[iconColor]} ${className}`}
-        >
-          <IconSVG className="w-2/3 h-2/3" />
-        </div>
-      );
-    } else {
-      return (
-        <IconSVG
-          data-tinafield={tinaField}
-          className={`${iconSizeClass[iconSize]} ${iconColorClasses} ${className}`}
-        />
-      );
-    }
-  }, [
-    parentColor,
-    data.style,
-    data.size,
-    data.color,
-    data.name,
-    IconSVG,
-    iconColor,
-  ]);
-
-  return Component;
-};
-
-const randomProperty = (obj) => {
-  let keys = Object.keys(obj);
-  return obj[keys[(keys.length * Math.random()) << 0]];
+  if (data.style == "circle") {
+    return (
+      <div
+        data-tinafield={tinaField}
+        className={`relative z-10 inline-flex items-center justify-center flex-shrink-0 ${iconSizeClass[iconSize]} rounded-full ${iconCircleColorClass[iconColor]} ${className}`}
+      >
+        <IconSVG className="w-2/3 h-2/3" />
+      </div>
+    );
+  } else {
+    return (
+      <IconSVG
+        data-tinafield={tinaField}
+        className={`${iconSizeClass[iconSize]} ${iconColorClasses} ${className}`}
+      />
+    );
+  }
 };
 
 export const iconSchema: TinaField = {
