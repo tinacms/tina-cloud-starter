f65cbea158d3660854cc68c65da157b3ce77c5e9
James O'Halloran <james.p.ohalloran@gmail.com>
2022-07-26 11:42:34 -0300

    rename TINA_TOKEN env

--- .tina/schema.ts
+++ .tina/schema.ts
@@ -12,7 +12,7 @@ const schema = defineSchema({
       process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
       process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
       process.env.HEAD!, // Netlify branch env
-    token: process.env.NEXT_PUBLIC_TINA_TOKEN!,
+    token: process.env.TINA_TOKEN!,
     media: {
       // If you wanted cloudinary do this
       // loadCustomStore: async () => {
