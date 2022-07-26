eebe048079cb5cf2aa116ffb198c2ba24073e4d5
James O'Halloran <james.p.ohalloran@gmail.com>
2022-07-26 10:01:36 -0300

    remove logs

--- .tina/schema.ts
+++ .tina/schema.ts
@@ -5,35 +5,13 @@ import { heroBlockSchema } from "../components/blocks/hero";
 import { testimonialBlockSchema } from "../components/blocks/testimonial";
 import { iconSchema } from "../components/util/icon";
 
-console.log(
-  "process.env.NEXT_PUBLIC_TINA_CLIENT_ID ",
-  process.env.NEXT_PUBLIC_TINA_CLIENT_ID?.charAt(1)
-);
-console.log(
-  "process.env.NEXT_PUBLIC_TINA_TOKEN ",
-  process.env.NEXT_PUBLIC_TINA_TOKEN?.charAt(1)
-);
-console.log(
-  "process.env.NEXT_PUBLIC_TINA_BRANCH ",
-  process.env.NEXT_PUBLIC_TINA_BRANCH
-);
-console.log("process.env.GITHUB_REF_NAME ", process.env.GITHUB_REF_NAME);
-console.log(
-  "branch ",
-  process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
-    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
-    process.env.HEAD! || // Netlify branch env
-    process.env.GITHUB_REF_NAME!
-);
-
 const schema = defineSchema({
   config: {
     clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
     branch:
       process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
       process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
-      process.env.HEAD! || // Netlify branch env
-      process.env.GITHUB_REF_NAME!, // GitHub action branch env
+      process.env.HEAD!,
     token: process.env.NEXT_PUBLIC_TINA_TOKEN!,
     media: {
       // If you wanted cloudinary do this
