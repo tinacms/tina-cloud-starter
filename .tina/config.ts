4199ba4fe961ebd37074074ed9630a7e8d520bc7
Logan Anderson <landerson2@upei.ca>
2022-08-09 14:34:35 -0400

    added static build

---
+++ .tina/config.ts
@@ -0,0 +1,60 @@
+import { defineConfig, RouteMappingPlugin } from "tinacms";
+import schema from "./schema";
+import { client } from "./__generated__/client";
+
+export default defineConfig({
+  build: {
+    outputFolder: "tina",
+    publicFolder: "public",
+  },
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
+  branch:
+    process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
+    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
+    process.env.HEAD!, // Netlify branch env
+  token: process.env.TINA_TOKEN!,
+  media: {
+    // If you wanted cloudinary do this
+    // loadCustomStore: async () => {
+    //   const pack = await import("next-tinacms-cloudinary");
+    //   return pack.TinaCloudCloudinaryMediaStore;
+    // },
+    // this is the config for the tina cloud media store
+    tina: {
+      publicFolder: "public",
+      mediaRoot: "uploads",
+    },
+  },
+  client,
+  schema,
+  cmsCallback: (cms) => {
+    /**
+     * When `tina-admin` is enabled, this plugin configures contextual editing for collections
+     */
+    const RouteMapping = new RouteMappingPlugin((collection, document) => {
+      if (["author", "global"].includes(collection.name)) {
+        return undefined;
+      }
+      if (["page"].includes(collection.name)) {
+        if (document._sys.filename === "home") {
+          return `/`;
+        }
+        if (document._sys.filename === "about") {
+          return `/about`;
+        }
+        return undefined;
+      }
+      return `/${collection.name}/${document._sys.filename}`;
+    });
+    cms.plugins.add(RouteMapping);
+
+    return cms;
+  },
+  formifyCallback: ({ formConfig, createForm, createGlobalForm }) => {
+    if (formConfig.id === "content/global/index.json") {
+      return createGlobalForm(formConfig);
+    }
+
+    return createForm(formConfig);
+  },
+});
