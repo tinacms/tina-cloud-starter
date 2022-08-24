493df6a67656c386e22deca17a9c77a9f9451d00
Jeff See <jeffsee.55@gmail.com>
2022-08-24 10:25:22 -0700

    Get things working with standalone iframe

---
+++ .tina/config.ts
@@ -0,0 +1,54 @@
+import { defineConfig, RouteMappingPlugin } from "tinacms";
+import { client } from "./__generated__/client";
+import schema from "./schema";
+
+export default defineConfig({
+  client,
+  schema,
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
+  branch:
+    process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
+    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
+    process.env.HEAD!, // Netlify branch env
+  token: process.env.TINA_TOKEN!,
+  media: {
+    tina: {
+      mediaRoot: "uploads",
+      publicFolder: "public",
+    },
+  },
+  cmsCallback: (cms) => {
+    /**
+     * When `tina-admin` is enabled, this plugin configures contextual editing for collections
+     */
+    const RouteMapping = new RouteMappingPlugin((collection, document) => {
+      if (["author", "global"].includes(collection.name)) {
+        return undefined;
+      }
+      if (["page"].includes(collection.name)) {
+        if (document._sys.filename === "home") {
+          return `/`;
+        }
+        if (document._sys.filename === "about") {
+          return `/about`;
+        }
+        return undefined;
+      }
+      return `/${collection.name}/${document._sys.filename}`;
+    });
+    cms.plugins.add(RouteMapping);
+
+    return cms;
+  },
+  formifyCallback: ({ formConfig, createForm, createGlobalForm }) => {
+    if (formConfig.id === "content/global/index.json") {
+      return createGlobalForm(formConfig);
+    }
+
+    return createForm(formConfig);
+  },
+});
--- .tina/schema.ts
+++ .tina/schema.ts
@@ -1,32 +1,11 @@
-import { defineSchema, defineConfig, RouteMappingPlugin } from "tinacms";
+import { defineSchema } from "tinacms";
 import { contentBlockSchema } from "../components/blocks/content";
 import { featureBlockSchema } from "../components/blocks/features";
 import { heroBlockSchema } from "../components/blocks/hero";
 import { testimonialBlockSchema } from "../components/blocks/testimonial";
 import { iconSchema } from "../components/util/icon";
-import { client } from "./__generated__/client";
 
-const schema = defineSchema({
-  config: {
-    clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
-    branch:
-      process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
-      process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
-      process.env.HEAD!, // Netlify branch env
-    token: process.env.TINA_TOKEN!,
-    media: {
-      // If you wanted cloudinary do this
-      // loadCustomStore: async () => {
-      //   const pack = await import("next-tinacms-cloudinary");
-      //   return pack.TinaCloudCloudinaryMediaStore;
-      // },
-      // this is the config for the tina cloud media store
-      tina: {
-        publicFolder: "public",
-        mediaRoot: "uploads",
-      },
-    },
-  },
+export default defineSchema({
   collections: [
     {
       label: "Blog Posts",
@@ -371,40 +350,3 @@ const schema = defineSchema({
     },
   ],
 });
-
-export const tinaConfig = defineConfig({
-  client,
-  schema,
-  cmsCallback: (cms) => {
-    /**
-     * When `tina-admin` is enabled, this plugin configures contextual editing for collections
-     */
-    const RouteMapping = new RouteMappingPlugin((collection, document) => {
-      if (["author", "global"].includes(collection.name)) {
-        return undefined;
-      }
-      if (["page"].includes(collection.name)) {
-        if (document._sys.filename === "home") {
-          return `/`;
-        }
-        if (document._sys.filename === "about") {
-          return `/about`;
-        }
-        return undefined;
-      }
-      return `/${collection.name}/${document._sys.filename}`;
-    });
-    cms.plugins.add(RouteMapping);
-
-    return cms;
-  },
-  formifyCallback: ({ formConfig, createForm, createGlobalForm }) => {
-    if (formConfig.id === "content/global/index.json") {
-      return createGlobalForm(formConfig);
-    }
-
-    return createForm(formConfig);
-  },
-});
-
-export default schema;
