507ce61b0ed4f9ff5a36a5ce3a789a46590b2a07
Scott Byrne <scott.byrne@forestry.io>
2022-12-09 16:37:22 -0400

    use color input for theme

--- components/fields/color.tsx
+++ components/fields/color.tsx
@@ -23,7 +23,7 @@ export const ColorPickerInput = wrapFieldsWithMeta(({ input }) => {
     red: "bg-red-500 border-red-600",
     pink: "bg-pink-500 border-pink-600",
     purple: "bg-purple-500 border-purple-600",
-    white: "bg-gray-50 border-gray-200",
+    white: "bg-white border-gray-150",
   };
 
   return (
