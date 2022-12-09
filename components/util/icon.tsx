17c4cff5da61e3718dfd06ec5e814c2365b145d9
Scott Byrne <scott.byrne@forestry.io>
2022-12-09 16:11:35 -0400

    refactor custom form inputs

--- components/util/icon.tsx
+++ components/util/icon.tsx
@@ -1,11 +1,10 @@
 import * as React from "react";
-import { GoCircleSlash } from "react-icons/go";
-import * as BoxIcons from "react-icons/bi";
+import { ColorPickerInput } from "../fields/color";
+import { IconPickerInput } from "../fields/icon";
 import { useTheme } from "../layout";
-import { Button, TinaField, wrapFieldsWithMeta } from "tinacms";
-import { Popover, Transition } from "@headlessui/react";
+import * as BoxIcons from "react-icons/bi";
 
-const IconOptions = {
+export const IconOptions = {
   Tina: (props) => (
     <svg
       {...props}
@@ -28,51 +27,42 @@ const IconOptions = {
 };
 
 const iconColorClass: {
-  [name: string]: { regular: string; input: string; circle: string };
+  [name: string]: { regular: string; circle: string };
 } = {
   blue: {
     regular: "text-blue-400",
-    input: "bg-blue-500 border-blue-600",
     circle: "bg-blue-400 dark:bg-blue-500 text-blue-50",
   },
   teal: {
     regular: "text-teal-400",
-    input: "bg-teal-500 border-teal-600",
     circle: "bg-teal-400 dark:bg-teal-500 text-teal-50",
   },
   green: {
     regular: "text-green-400",
-    input: "bg-green-500 border-green-600",
     circle: "bg-green-400 dark:bg-green-500 text-green-50",
   },
   red: {
     regular: "text-red-400",
-    input: "bg-red-500 border-red-600",
     circle: "bg-red-400 dark:bg-red-500 text-red-50",
   },
   pink: {
     regular: "text-pink-400",
-    input: "bg-pink-500 border-pink-600",
     circle: "bg-pink-400 dark:bg-pink-500 text-pink-50",
   },
   purple: {
     regular: "text-purple-400",
-    input: "bg-purple-500 border-purple-600",
     circle: "bg-purple-400 dark:bg-purple-500 text-purple-50",
   },
   orange: {
     regular: "text-orange-400",
-    input: "bg-orange-500 border-orange-600",
     circle: "bg-orange-400 dark:bg-orange-500 text-orange-50",
   },
   yellow: {
     regular: "text-yellow-400",
-    input: "bg-yellow-500 border-yellow-600",
     circle: "bg-yellow-400 dark:bg-yellow-500 text-yellow-50",
   },
   white: {
     regular: "text-white opacity-80",
-    input: "bg-white border-gray-100",
     circle: "bg-white-400 dark:bg-white-500 text-white-50",
   },
 };
@@ -139,162 +129,7 @@ export const Icon = ({
   }
 };
 
-const formatFieldLabel = (value: string) => {
-  return value.charAt(0).toUpperCase() + value.slice(1);
-};
-
-const parseIconName = (name: string) => {
-  const splitName = name.split(/(?=[A-Z])/);
-  if (splitName.length > 1) {
-    return splitName.slice(1).join(" ");
-  } else {
-    return name;
-  }
-};
-
-const IconPickerInput = wrapFieldsWithMeta(({ input }) => {
-  const [filter, setFilter] = React.useState("");
-  const filteredBlocks = React.useMemo(() => {
-    return Object.keys(IconOptions).filter((name) => {
-      return name.toLowerCase().includes(filter.toLowerCase());
-    });
-  }, [filter]);
-
-  const inputLabel = Object.keys(IconOptions).includes(input.value)
-    ? parseIconName(input.value)
-    : "Select Icon";
-  const InputIcon = IconOptions[input.value] ? IconOptions[input.value] : null;
-
-  return (
-    <div className="relative z-[1000]">
-      <input type="text" id={input.name} className="hidden" {...input} />
-      <Popover>
-        {({ open }) => (
-          <>
-            <Popover.Button as={"span"}>
-              <Button
-                className={`text-sm h-11 px-4 ${InputIcon ? "h-11" : "h-10"}`}
-                size="custom"
-                rounded="full"
-                variant={open ? "secondary" : "white"}
-              >
-                {InputIcon && (
-                  <InputIcon className="w-7 mr-1 h-auto fill-current text-blue-500" />
-                )}
-                {inputLabel}
-                {!InputIcon && (
-                  <BoxIcons.BiChevronRight className="w-5 h-auto fill-current opacity-70 ml-1" />
-                )}
-              </Button>
-            </Popover.Button>
-            <div className="absolute w-full -bottom-2 left-0 translate-y-full">
-              <Transition
-                enter="transition duration-150 ease-out"
-                enterFrom="transform opacity-0 -translate-y-2"
-                enterTo="transform opacity-100 translate-y-0"
-                leave="transition duration-75 ease-in"
-                leaveFrom="transform opacity-100 translate-y-0"
-                leaveTo="transform opacity-0 -translate-y-2"
-              >
-                <Popover.Panel className="relative overflow-hidden rounded-lg shadow-lg bg-white border border-gray-150 z-50">
-                  {({ close }) => (
-                    <div className="min-w-[192px] max-h-[24rem] flex flex-col w-full h-full">
-                      <div className="bg-gray-50 p-2 border-b border-gray-100 z-10 shadow-sm">
-                        <input
-                          type="text"
-                          className="bg-white text-sm rounded-sm border border-gray-100 shadow-inner py-1.5 px-2.5 w-full block placeholder-gray-200"
-                          onClick={(event: any) => {
-                            event.stopPropagation();
-                            event.preventDefault();
-                          }}
-                          value={filter}
-                          onChange={(event: any) => {
-                            setFilter(event.target.value);
-                          }}
-                          placeholder="Filter..."
-                        />
-                      </div>
-                      {filteredBlocks.length === 0 && (
-                        <span className="relative text-center text-xs px-2 py-3 text-gray-300 bg-gray-50 italic">
-                          No matches found
-                        </span>
-                      )}
-                      {filteredBlocks.length > 0 && (
-                        <div className="w-full grid grid-cols-6 auto-rows-auto p-2 overflow-y-auto">
-                          <button
-                            className="relative rounded-lg text-center text-xs py-2 px-3 flex-1 outline-none transition-all ease-out duration-150 hover:text-blue-500 focus:text-blue-500 focus:bg-gray-50 hover:bg-gray-50"
-                            key={"clear-input"}
-                            onClick={() => {
-                              input.onChange("");
-                              setFilter("");
-                              close();
-                            }}
-                          >
-                            <GoCircleSlash className="w-6 h-auto text-gray-200" />
-                          </button>
-                          {filteredBlocks.map((name) => {
-                            return (
-                              <button
-                                className="relative rounded-lg text-center text-xs py-2 px-3 flex-1 outline-none transition-all ease-out duration-150 hover:text-blue-500 focus:text-blue-500 focus:bg-gray-50 hover:bg-gray-50"
-                                key={name}
-                                onClick={() => {
-                                  input.onChange(name);
-                                  setFilter("");
-                                  close();
-                                }}
-                              >
-                                <Icon
-                                  data={{
-                                    name: name,
-                                    size: "custom",
-                                    color: "blue",
-                                  }}
-                                  className="w-7 h-auto"
-                                />
-                              </button>
-                            );
-                          })}
-                        </div>
-                      )}
-                    </div>
-                  )}
-                </Popover.Panel>
-              </Transition>
-            </div>
-          </>
-        )}
-      </Popover>
-    </div>
-  );
-});
-
-const ColorPickerInput = wrapFieldsWithMeta(({ input }) => {
-  return (
-    <>
-      <input type="text" id={input.name} className="hidden" {...input} />
-      <div className="flex gap-2 flex-wrap">
-        {Object.keys(iconColorClass).map((color) => {
-          return (
-            <button
-              className={`w-9 h-9 rounded-full shadow border ${
-                iconColorClass[color].input
-              } ${
-                input.value === color
-                  ? "ring-[3px] ring-offset-2 ring-blue-400"
-                  : ""
-              }`}
-              onClick={() => {
-                input.onChange(color);
-              }}
-            ></button>
-          );
-        })}
-      </div>
-    </>
-  );
-});
-
-export const iconSchema: TinaField = {
+export const iconSchema = {
   type: "object",
   label: "Icon",
   name: "icon",
