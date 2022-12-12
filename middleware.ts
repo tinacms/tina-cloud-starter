f049dadf98e806c5b53309620d329a155ac1bd86
James O'Halloran <james.p.ohalloran@gmail.com>
2022-12-12 17:46:50 -0400

    check on cookies instead of auth header

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,22 @@
 // middleware.ts
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { isAuthorized } from "@tinacms/auth";
 
 // This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest) {
-  // return NextResponse.next();
-
-  const mockedReq = {
-    query: { clientID: process.env.NEXT_PUBLIC_TINA_CLIENT_ID },
-    headers: request.headers,
-  };
-  if (!(await isAuthorized(mockedReq as any))) {
+export function middleware(request: NextRequest) {
+  if (!request.cookies.get("_ga")) {
     return NextResponse.next();
   }
 
   const dest = request.headers.get("sec-fetch-dest");
 
   const pathName = new URL(request.url).pathname;
-  // console.log("url", dest, pathName, request.headers.values());
 
-  if (dest == "iframe") {
-    console.log("iframe rewrite", pathName, request.url);
-    return NextResponse.rewrite(new URL(pathName, request.url));
-  }
+  //This rewrite doesn't seem to work
+  // if (dest == "iframe") {
+  //   console.log("iframe rewrite", pathName, request.url);
+  //   return NextResponse.rewrite(new URL(pathName, request.url));
+  // }
   if (dest == "document") {
     console.log(
       `document rewrite\nfrom: ${pathName} to ${
@@ -31,11 +24,8 @@ export async function middleware(request: NextRequest) {
       }`
     );
     return NextResponse.rewrite(new URL("/admin", request.url));
-    //return NextResponse.rewrite(new URL("/admin#/~" + pathName, request.url));
   }
   return NextResponse.next();
-
-  // console.log("foo", new URL("/admin#/~" + pathName, request.url).href);
 }
 
 // See "Matching Paths" below to learn more
