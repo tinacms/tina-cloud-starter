879773a4b0d73de0319aa42216ab0c480e5c3d9d
Jeff See <jeffsee.55@gmail.com>
2023-03-27 12:01:54 -0700

    update client imports

--- graphql.config.js
+++ graphql.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   projects: {
     app: {
-      schema: [".tina/__generated__/schema.gql"],
+      schema: ["tina/__generated__/schema.gql"],
       documents: [
-        ".tina/__generated__/queries.gql",
-        ".tina/__generated__/frags.gql",
-        ".tina/queries/queries.gql",
-        ".tina/queries/frags.gql",
+        "tina/__generated__/queries.gql",
+        "tina/__generated__/frags.gql",
+        "tina/queries/queries.gql",
+        "tina/queries/frags.gql",
         "pages/**/*.{graphql,js,ts,jsx,tsx}",
       ],
     },
