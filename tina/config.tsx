adcdecf511c6774a8876dd54edcc7441edfe41f1
Jeff See <jeffsee.55@gmail.com>
2023-05-05 09:34:46 -0700

    Try self-hosting for demoing

--- tina/config.tsx
+++ tina/config.tsx
@@ -13,6 +13,11 @@ const config = defineConfig({
     process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
     process.env.HEAD!, // Netlify branch env
   token: process.env.TINA_TOKEN!,
+  admin: {
+    auth: {
+      useLocalAuth: true,
+    },
+  },
   media: {
     // If you wanted cloudinary do this
     // loadCustomStore: async () => {
