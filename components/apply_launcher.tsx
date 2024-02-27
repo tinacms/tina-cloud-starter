53e3e6b5729e94bcc05fe1f2032bd82e4f162e03
Archit <archit@jobvite-inc.com>
2024-02-27 10:44:39 -0500

    add launcher

--- components/apply_launcher.tsx
+++ components/apply_launcher.tsx
@@ -1,6 +1,8 @@
-import React, {  useEffect } from 'react';
+import React, {  useEffect, useState, useCallback } from 'react';
 
 const ApplyLauncherComponent = () => {
+
+  const [data, setData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -9,15 +11,45 @@ const ApplyLauncherComponent = () => {
         const jobId = 4;
         const guid = "2fec6866-5d76-48e6-a140-c819e96863ff";
         const response = await fetch(`${apiUrl}/job/${jobId}/get_apply_launcher/${guid}`);
-        const apply_launcher = await response.json();
-        <div dangerouslySetInnerHTML={{ __html: apply_launcher.link }} />;
+        const launcherData = await response.json();
+        if (launcherData?.error) {
+          setData({ error: launcherData.error });
+          return;
+        }
+        setData({
+          applyKey: launcherData?.apply_key,
+          jobId: launcherData?.job_id,
+          workflowType: launcherData?.workflow_type,
+          widgetOptions: launcherData?.widget_options
+        });
       } catch (error) {
-        return <div>Error fetching data: {error}</div>;
+        console.log(error);
+        setData(null);
+        return;
       }
     };
 
     fetchData();
-  }, []);
+  }, [data]);
+
+  const handleClick = useCallback(() => {
+    if (data?.applyKey && global.CareerSite !== undefined) {
+      global.CareerSite.Apply.launchApplicationWorkflow(data?.applyKey, data?.jobId, data?.workflowType, data?.widgetOptions);
+    }
+  }, [data]);
+
+  if (data?.error) {
+    return <div>{data.error}</div>
+  } 
+  
+  if (data?.applyKey) {
+    return (
+      <button type="button" onClick={handleClick}>Launch</button>
+    );
+  }
+
+  return null;
+  
 };
 
 export default ApplyLauncherComponent;
