067685ad17be68db47c527c3d9a395c1ff60432e
joshua berman <jber4409@uni.sydney.edu.au>
2024-05-27 11:56:04 +1000

    Add editor seciton to tina starter

--- components/blocks/hero.tsx
+++ components/blocks/hero.tsx
@@ -27,7 +27,7 @@ export const Hero = ({ data }: { data: PageBlocksHero }) => {
         size="large"
         className="grid grid-cols-1 md:grid-cols-5 gap-14 items-center justify-center"
       >
-        <div className="row-start-2 md:row-start-1 md:col-span-3 text-center md:text-left">
+        <div className="row-start-2 md:row-start-1 md:col-span-5 text-center md:text-left">
           {data.tagline && (
             <h2
               data-tina-field={tinaField(data, "tagline")}
@@ -40,7 +40,7 @@ export const Hero = ({ data }: { data: PageBlocksHero }) => {
           {data.headline && (
             <h3
               data-tina-field={tinaField(data, "headline")}
-              className={`w-full relative	mb-10 text-5xl font-extrabold tracking-normal leading-tight title-font`}
+              className={`w-full relative mb-10 text-5xl font-extrabold tracking-normal leading-tight title-font`}
             >
               <span
                 className={`bg-clip-text text-transparent bg-gradient-to-r  ${
@@ -53,16 +53,28 @@ export const Hero = ({ data }: { data: PageBlocksHero }) => {
               </span>
             </h3>
           )}
-          {data.text && (
-            <div
-              data-tina-field={tinaField(data, "text")}
-              className={`prose prose-lg mx-auto md:mx-0 mb-10 ${
-                data.color === "primary" ? `prose-primary` : `dark:prose-dark`
-              }`}
-            >
-              <TinaMarkdown content={data.text} />
-            </div>
-          )}
+          <div className="flex flex-col md:flex-row gap-6">
+            {data.text && (
+              <div
+                data-tina-field={tinaField(data, "text")}
+                className={`prose prose-lg mx-auto md:mx-0 mb-10 ${
+                  data.color === "primary" ? `prose-primary` : `dark:prose-dark`
+                }`}
+              >
+                <TinaMarkdown content={data.text} />
+              </div>
+            )}
+            {data.text2 && (
+              <div
+                data-tina-field={tinaField(data, "text2")}
+                className={`prose prose-lg mx-auto md:mx-0 mb-10 ${
+                  data.color === "primary" ? `prose-primary` : `dark:prose-dark`
+                }`}
+              >
+                <TinaMarkdown content={data.text2} />
+              </div>
+            )}
+          </div>
           {data.actions && (
             <Actions
               className="justify-center md:justify-start py-2"
@@ -74,18 +86,14 @@ export const Hero = ({ data }: { data: PageBlocksHero }) => {
         {data.image && (
           <div
             data-tina-field={tinaField(data.image, "src")}
-            className="relative row-start-1 md:col-span-2 flex justify-center"
+            className="relative row-start-3 md:col-span-5 flex justify-center mt-10"
           >
             <img
-              className="absolute w-full rounded-lg max-w-xs md:max-w-none h-auto blur-2xl brightness-150 contrast-[0.9] dark:brightness-150 saturate-200 opacity-50 dark:opacity-30 mix-blend-multiply dark:mix-blend-hard-light"
+              className="w-full rounded-lg max-w-xs h-auto"              
               src={data.image.src}
               aria-hidden="true"
             />
-            <img
-              className="relative z-10 w-full max-w-xs rounded-lg md:max-w-none h-auto"
-              alt={data.image.alt}
-              src={data.image.src}
-            />
+
           </div>
         )}
       </Container>
@@ -116,7 +124,12 @@ export const heroBlockSchema: TinaTemplate = {
       name: "headline",
     },
     {
-      label: "Text",
+      type: "rich-text",
+      label: "Text-2",
+      name: "text2",
+    },
+    {
+      label: "Text-1",
       name: "text",
       type: "rich-text",
     },
