e5a24df35688d0084ff4a172fca6c63b8ccd3cc2
Dylan Burkey <dylanburkey@outlook.com>
2024-10-21 18:48:12 -0400

    Update tailwind.config.ts
    Update sceen sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -57,9 +57,9 @@ module.exports = {
       },
     },
     screens: {
-      sm: "600px",
-      md: "900px",
-      lg: "1200px",
+      sm: "650px",
+      md: "1100px",
+      lg: "13200px",
       xl: "1500px",
       "2xl": "1800px",
     },
