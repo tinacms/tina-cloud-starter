86bb67e0f25acdf92e9f3c1745eb0258c45346e7
benjaminneoh <benjaminneoh6343@gmail.com>
2025-01-03 10:42:31 +1100

    fix next themes not found error

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
