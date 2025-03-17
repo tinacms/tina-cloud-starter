03052ce468acc7e8cf4e0676b56215182a77850c
Matt Wicks <matt.wicks@gmail.com>
2025-03-17 18:14:17 +1100

    add revalidation for dynamic pages with a 300-second cache

--- app/[...urlSegments]/page.tsx
+++ app/[...urlSegments]/page.tsx
@@ -3,6 +3,8 @@ import client from '@/tina/__generated__/client';
 import Layout from '@/components/layout/layout';
 import ClientPage from './client-page';
 
+export const revalidate = 300;
+
 export default async function Page({
   params,
 }: {
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -2,6 +2,8 @@ import Layout from '@/components/layout/layout';
 import client from '@/tina/__generated__/client';
 import PostsClientPage from './client-page';
 
+export const revalidate = 300;
+
 export default async function PostsPage() {
   let posts = await client.queries.postConnection({
     sort: 'date',
