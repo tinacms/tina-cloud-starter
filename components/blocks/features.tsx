d5f52b58f8e73fbb630f2bd95c67aba0119c515b
Matt Wicks <matt.wicks@gmail.com>
2025-03-31 16:31:53 +1100

    update features block

--- components/blocks/features.tsx
+++ components/blocks/features.tsx
@@ -1,5 +1,6 @@
 "use client";
 import {
+  Page,
   PageBlocksFeatures,
   PageBlocksFeaturesItems,
 } from "../../tina/__generated__/types";
@@ -10,60 +11,65 @@ import { Section } from "../layout/section";
 import { Container } from "../layout/container";
 import { iconSchema } from "../../tina/fields/icon";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
+import { Card, CardContent, CardHeader } from "../ui/card";
 
-export const Feature = ({
-  featuresColor,
-  data,
-}: {
-  featuresColor: string;
-  data: PageBlocksFeaturesItems;
-}) => {
+export const Features = ({ data }: { data: PageBlocksFeatures }) => {
+  console.log('data', data);
   return (
-    <div
-      data-tina-field={tinaField(data)}
-      className="flex-1 flex flex-col gap-6 text-center items-center lg:items-start lg:text-left max-w-xl mx-auto"
-      style={{ flexBasis: "16rem" }}
-    >
-      {data.icon && (
-        <Icon
-          tinaField={tinaField(data, "icon")}
-          parentColor={featuresColor}
-          data={{ size: "large", ...data.icon }}
-        />
-      )}
-      {data.title && (
+    <section className="bg-zinc-50 py-16 md:py-32 dark:bg-transparent">
+      <div className="@container mx-auto max-w-5xl px-6">
+        <div className="text-center">
+          <h2 className="text-balance text-4xl font-semibold lg:text-5xl">Built to cover your needs</h2>
+          <p className="mt-4">Libero sapiente aliquam quibusdam aspernatur, praesentium iusto repellendus.</p>
+        </div>
+        <Card className="@min-4xl:max-w-full @min-4xl:grid-cols-3 @min-4xl:divide-x @min-4xl:divide-y-0 mx-auto mt-8 grid max-w-sm divide-y overflow-hidden shadow-zinc-950/5 *:text-center md:mt-16">
+          {data.items &&
+            data.items.map(function (block, i) {
+              return <Feature key={i} {...block!} />;
+            })}
+        </Card>
+      </div>
+    </section>
+  )
+}
+
+const CardDecorator = ({ children }: { children: React.ReactNode }) => (
+  <div className="relative mx-auto size-36 duration-200 [--color-border:color-mix(in_oklab,var(--color-zinc-950)10%,transparent)] group-hover:[--color-border:color-mix(in_oklab,var(--color-zinc-950)20%,transparent)] dark:[--color-border:color-mix(in_oklab,var(--color-white)15%,transparent)] dark:group-hover:bg-white/5 dark:group-hover:[--color-border:color-mix(in_oklab,var(--color-white)20%,transparent)]">
+    <div aria-hidden className="absolute inset-0 bg-[linear-gradient(to_right,var(--color-border)_1px,transparent_1px),linear-gradient(to_bottom,var(--color-border)_1px,transparent_1px)] bg-[size:24px_24px]" />
+    <div aria-hidden className="bg-radial to-background absolute inset-0 from-transparent to-75%" />
+    <div className="bg-background absolute inset-0 m-auto flex size-12 items-center justify-center border-l border-t">{children}</div>
+  </div>
+)
+
+export const Feature: React.FC<PageBlocksFeaturesItems> = (data) => {
+  return (
+    <div className="group shadow-zinc-950/5">
+      <CardHeader className="pb-3">
+        <CardDecorator>
+          {/* <Zap className="size-6" aria-hidden /> */}
+          {data.icon && (
+            <Icon
+              tinaField={tinaField(data, "icon")}
+              data={{ size: "large", ...data.icon }}
+            />
+          )}
+        </CardDecorator>
+
         <h3
           data-tina-field={tinaField(data, "title")}
-          className="text-2xl font-semibold title-font"
+          className="mt-6 font-medium"
         >
           {data.title}
         </h3>
-      )}
-      {data.text && (
-        <div className="prose text-base opacity-80 leading-relaxed">
-          <TinaMarkdown
-            data-tina-field={tinaField(data, "text")}
-            content={data.text}
-          />
-        </div>
-      )}
-    </div>
-  );
-};
+      </CardHeader>
 
-export const Features = ({ data }: { data: PageBlocksFeatures }) => {
-  return (
-    <Section color={data.color}>
-      <Container
-        className={`flex flex-wrap gap-x-10 gap-y-8 text-left`}
-        size="large"
-      >
-        {data.items &&
-          data.items.map(function (block, i) {
-            return <Feature featuresColor={data.color!} key={i} data={block!} />;
-          })}
-      </Container>
-    </Section>
+      <CardContent className="text-sm">
+        <TinaMarkdown
+          data-tina-field={tinaField(data, "text")}
+          content={data.text}
+        />
+      </CardContent>
+    </div>
   );
 };
 
@@ -103,7 +109,7 @@ export const featureBlockSchema: Template = {
         },
       },
       fields: [
-        iconSchema,
+        iconSchema as any,
         {
           type: "string",
           label: "Title",
@@ -114,16 +120,6 @@ export const featureBlockSchema: Template = {
           label: "Text",
           name: "text",
         },
-      ],
-    },
-    {
-      type: "string",
-      label: "Color",
-      name: "color",
-      options: [
-        { label: "Default", value: "default" },
-        { label: "Tint", value: "tint" },
-        { label: "Primary", value: "primary" },
       ],
     },
   ],
